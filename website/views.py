49a765148f3c49c02f36eac49e7222a93535eba9
parsashky <mohammadrezaparsaii@gmail.com>
2026-05-14 14:16:25 +0330

    created render html files

--- website/views.py
+++ website/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse , JsonResponse
 
 
 def index_view(request):
-    return HttpResponse('<h1>home page</h1>')
+    return render(request,'website/index.html')
 def about_view(request):
-    return HttpResponse('<h1>about page</h1>')
+    return render(request,'website/about.html')
 def contact_view(request):
-    return HttpResponse('<h1>contact page</h1>')
+    return render(request,'website/contact.html')
