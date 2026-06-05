46f8fa72c1a3b6d01b28043306edc11532d7be10
parsashky <mohammadrezaparsaii@gmail.com>
2026-06-05 20:52:38 +0330

    modified post-category file and blog-tag and created function for post categories

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import post
+from blog.models import Category
 
 
 register = template.Library()
@@ -7,3 +8,12 @@ register = template.Library()
 @register.inclusion_tag("blog/latest-post.html")
 def latestpost():
     posts = post.objects.filter(status=True).order_by('published_date')[:3]
+
+@register.inclusion_tag("blog/post-category.html")
+def postcategories():
+    posts = post.objects.filter(status=True)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category = name).count()
+    return {'categories' : cat_dict}
