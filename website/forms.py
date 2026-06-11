1bbd372d6f460b6443a1c25c10e26729b95d2c85
parsashky <mohammadrezaparsaii@gmail.com>
2026-06-11 20:06:39 +0330

    added forms file in my project

---
+++ website/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class NameForm(forms.Form):
+    first_name = forms.CharField(max_length=100)
+    subject = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
