63184139921b75c88fce4e01ab2ef543363878a5
parsashky <mohammadrezaparsaii@gmail.com>
2026-06-12 16:39:07 +0330

    completed form in my project

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect , JsonResponse
 from website.forms import contactform , NameForm , newsletterform
+from django.contrib import messages
 
 
 def index_view(request):
@@ -12,6 +13,9 @@ def contact_view(request):
         form = contactform(request.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(request,messages.SUCCESS,'your ticket submited successfully')
+        else:
+            messages.add_message(request,messages.ERROR,'your ticket did not submited')
     form = contactform()
     return render(request,'website/contact.html',{'form':form})
 def newsletter_view(request):
