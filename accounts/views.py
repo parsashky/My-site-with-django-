5c696d19173c1ef05a7c5257b1a505ab29ae384a
parsashky <mohammadrezaparsaii@gmail.com>
2026-06-20 15:15:52 +0330

    added login_required in project and modified login-view

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,33 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate,login,logout
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request,username=username, password=password)
-        if user is not None:
-            login(request,user)
-            return redirect('/')
-    return render(request,'accounts/login.html')
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get("username")
+                password = form.cleaned_data.get("password")
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+        form = AuthenticationForm()
+    else:
+        return redirect("/")
+
+    return render(request, "accounts/login.html", {"form": form})
+
+@login_required
 def logout_view(request):
-    pass
+    logout(request)
+    return redirect('/')
+
+
 def signup_view(request):
-    return render(request,'accounts/signup.html')
+    return render(request, "accounts/signup.html")
